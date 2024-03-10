54512bc7a1fc0789aa10f0cad3faf83aab80d8b9
rashid geelani <syed.geelani@vit.edu.in>
2024-03-10 23:36:24 +0530

    error

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,36 @@
-import { Inter } from "next/font/google";
-import Layout from "./components/layout/Layout";
+// @ts-nocheck
 
-const inter = Inter({ subsets: ["latin"] });
+import Header from "./components/layout/Header";
+import themes from "./components/layout/themes";
+import styled, {ThemeProvider, createGlobalStyle} from 'styled-components'
+import {useState, createContext} from 'react-server-dom-webpack/server.edge'
+
+
 
 export const metadata = {
   title: "gogrowfund.com",
-  description: "developed by Syed Rashid Geelani, Saurabh Yamgar and Saurabh Pandey",
+  description: " cdeveloped by Syed Rashid Geelani, Saurabh Yamgar and Saurabh Pandey",
 };
 
+
 export default function RootLayout({ children }) {
+  const [theme, setTheme] = useState('light');
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Layout />
+    <ThemeProvider theme={themes[theme]}>
+      <LayoutWrapper onClick={changeTheme}>
+        <Header />
         {children}
-        </body>
-    </html>
+      </LayoutWrapper>
+    </ThemeProvider>   
   );
+}
+
+
+const LayoutWrapper = styled.div`
+  backgrounf-color: ${(props)=> props.theme.bgColor};
+  background-image: ${(props) => props.theme.bgImage};
+  color: ${(props)=> props.theme.color};
+`
+const changeTheme = ()=>{
+  setTheme(theme == "light" ? "dark" : "light");
 }
